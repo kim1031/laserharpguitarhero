498f1a37d7f4f71de02d7190c95623e8a969a8c7
Julia Gonik <jgonik@mit.edu>
2019-04-20 17:48:47 -0400

    esp32 code to detect when laser is broken

---
+++ laser/laser.ino
@@ -0,0 +1,27 @@
+#include <SPI.h>
+#include <TFT_eSPI.h>
+
+TFT_eSPI tft = TFT_eSPI();
+
+void setup() {
+  Serial.begin(115200); // Set up serial port
+  tft.init();  //init screen
+  tft.setRotation(2); //adjust rotation
+  tft.setTextSize(1); //default font size
+  tft.fillScreen(TFT_WHITE); //fill background
+  tft.setTextColor(TFT_BLACK, TFT_WHITE); //set color for font
+
+}
+
+void loop() {
+  int bins = analogRead(A16);
+  float voltage = (bins/4096.0)*3.3;
+
+  if(voltage < 0.15) {
+    Serial.println("laser unbroken");   
+  }
+  else{
+    Serial.println("laser broken");
+  }
+
+}
