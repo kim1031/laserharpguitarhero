903dce3bcd47bffd7cae5dda018900e13e17aa70
Julia Gonik <jgonik@mit.edu>
2019-05-16 21:43:59 -0400

    commented song selection

--- laser_harp_hero/SongSelection.cpp
+++ laser_harp_hero/SongSelection.cpp
@@ -10,12 +10,12 @@
 #include <string>
 
 SongSelection::SongSelection() {
-  this->curr_index = 0;
-  this->array_size = 0;
-  this->old_index = 1;
-  this->scrolling_timer = millis();
+  this->curr_index = 0; //stores current position in songs, artists, and durations arrays
+  this->array_size = 0; //keep track of array size
+  this->scrolling_timer = millis(); //initialize scrolling timer
 }
 
+//creates the request buffer for the get request that retrieves the song list from the server.
 void SongSelection::get_song_selection(char* request_buffer) {
   sprintf(request_buffer, "GET http://608dev.net/sandbox/sc/jgonik/laserharpguitarhero/get_songlist.py");
   strcat(request_buffer, " HTTP/1.1\r\n");
@@ -23,17 +23,18 @@ void SongSelection::get_song_selection(char* request_buffer) {
   strcat(request_buffer, "\r\n");
 }
 
+//parses the response from the get request
 void SongSelection::parse_song_selection(std::string str) { //pass in response buffer but as a string
-  int array_index = 0;
-  while (str.size() > 0) {
-    int name_start = str.find("\'");
-    if (name_start != -1) {
+  int array_index = 0; //start at index zero
+  while (str.size() > 0) { 
+    int name_start = str.find("\'"); //find the location of the first aostrophe, which signifies the start of the name
+    if (name_start != -1) { //if we found the apostrophe...
       str = str.substr(name_start+1);
-      int name_end = str.find("\'");
+      int name_end = str.find("\'"); //find the next apostrophe, which signifies the end of the name
       if (name_end != -1) {
-        std::string song_name = str.substr(0, name_end);
+        std::string song_name = str.substr(0, name_end); //create a substring of just the name and add it to the song name array
         songs[array_index] = song_name;
-        str = str.substr(name_end+4);
+        str = str.substr(name_end+4); //remove song name from start of string
       }
       else {
         break;
@@ -42,30 +43,30 @@ void SongSelection::parse_song_selection(std::string str) { //pass in response b
     else {
       break;
     }
-    int artist_end = str.find("\'");
+    int artist_end = str.find("\'"); //find the next apostrophe, which signifies the end of the artist's name
     if (artist_end != -1) {
-      std::string artist_name = str.substr(0, artist_end);
+      std::string artist_name = str.substr(0, artist_end); //get the substring of just the artist's name and add it to the artists array
       artists[array_index] = artist_name;
-      str = str.substr(artist_end + 3);
+      str = str.substr(artist_end + 3); //remove artist name from start of string
     }
     else {
       break;
     }
-    int duration_end = str.find(")");
+    int duration_end = str.find(")"); //find the next closing parentheses, which signify the end of the duration
     if (duration_end != -1) {
-      float dur = atof(str.substr(0, duration_end).c_str());
+      float dur = atof(str.substr(0, duration_end).c_str()); //get a substring of just the duration and convert it to a float before adding it to the durations array
       durations[array_index] = dur;
     }
     else {
       break;
     }
-    array_index++;
+    array_index++; //increase array index
   }
-  array_size = array_index;
+  array_size = array_index; //at the end, the final array index is the size of the array
 }
 
+//updates the current index. takes in a boolean forward, which is true if we are scrolling forwards and false if we are scrolling backwards
 void SongSelection::update_song_index(bool forward) {
-  old_index = curr_index;
   if (forward) {
     curr_index++;
   }
@@ -73,73 +74,66 @@ void SongSelection::update_song_index(bool forward) {
     curr_index--;
   }
 
-  if (curr_index >= array_size) {
+  if (curr_index >= array_size) { //if the current index is greater than or equal to the array size, we set it to zero so that we scroll cyclically
     curr_index = 0;
   }
-  else if (curr_index < 0) {
+  else if (curr_index < 0) { //if the current index is less than zero, we set it to the maximum possible index so that we scroll cyclically
     curr_index = array_size - 1;
   }
 }
 
 void SongSelection::display_song_selection(Adafruit_RA8875* tft) {
-  //tft->fillScreen(RA8875_BLACK); //do we need this we'll see
-  Serial.println("drawing the damn rectangle");
-  tft->fillRect(0, 10, 400, 70, RA8875_BLACK);
+  //display song information to TFT screen
+  tft->fillRect(0, 10, 400, 70, RA8875_BLACK); //draw black rectangle to cover previous information
   tft->textTransparent(RA8875_CYAN);
   tft->textSetCursor(0, 0);
-  tft->textWrite("Song Selection");
+  tft->textWrite("Song Selection"); //title
   tft->textMode();
   tft->textSetCursor(10, 20);
-  tft->textWrite(songs[curr_index].c_str());
+  tft->textWrite(songs[curr_index].c_str()); //display current song name
   tft->textSetCursor(10, 40);
-  tft->textWrite(artists[curr_index].c_str());
+  tft->textWrite(artists[curr_index].c_str()); //display current artist name
   tft->textSetCursor(10, 60);
   float curr_dur = durations[curr_index];
   char dur_str[30];
   sprintf(dur_str, "%f", curr_dur);
   strcat(dur_str, " seconds");
-  tft->textWrite(dur_str);
+  tft->textWrite(dur_str); //display current duration as a string
 }
 
 std::string SongSelection::get_curr_song() {
-  return songs[curr_index];
+  return songs[curr_index]; //get current song name
 }
 
+//updates the screen based on which laser is selected
+//input is a laser number (one being leftmost), tft is a pointer to the tft object
 void SongSelection::update_screen(int input, Adafruit_RA8875* tft) {
   switch(input){
     case 1: {  //scroll left
-     if (millis() - scrolling_timer >= scrolling_threshold) {
-      old_song = selected_song;
-      update_song_index(false);
+     if (millis() - scrolling_timer >= scrolling_threshold) { //only scroll if enough time has elapsed
+      old_song = selected_song; //keep track of old song
+      update_song_index(false); //scroll backwards
       selected_song = get_curr_song();
-      if (old_song.compare(selected_song) != 0) {
-         Serial.print("old song ");
-          Serial.println(old_song.c_str());
-          Serial.print("curr song ");
-          Serial.println(selected_song.c_str());
+      if (old_song.compare(selected_song) != 0) { //only draw if old song is different from new song
         display_song_selection(tft);
       }     
-      scrolling_timer = millis();
+      scrolling_timer = millis(); //reset scrolling timer
      }
      break;
     }
     case 2: { //scroll right
-      if (millis() - scrolling_timer >= scrolling_threshold) {
+      if (millis() - scrolling_timer >= scrolling_threshold) { //only scroll if enough time has elapsed
         old_song = selected_song;
         update_song_index(true);
         selected_song = get_curr_song();
-        if (old_song.compare(selected_song) != 0) {
-          Serial.print("old song ");
-          Serial.println(old_song.c_str());
-          Serial.print("curr song ");
-          Serial.println(selected_song.c_str());
+        if (old_song.compare(selected_song) != 0) { //only draw if old song is different from current song
           display_song_selection(tft);
         }
-        scrolling_timer = millis();
+        scrolling_timer = millis(); //reset scrolling timer
       }
       break;
     }
-   case 3: { //selected song
+   case 3: { //user selected song. display choice to screen
     selected_song = get_curr_song();
     tft->fillRect(0, 0, 800, 400, RA8875_BLACK);
     tft->textTransparent(RA8875_CYAN);
@@ -150,7 +144,6 @@ void SongSelection::update_screen(int input, Adafruit_RA8875* tft) {
     strcat(display_text, selected_song.c_str());
     tft->textSetCursor(10, 40);
     tft->textWrite(display_text);
-    //choosing = false;
     break;
    }
   }
